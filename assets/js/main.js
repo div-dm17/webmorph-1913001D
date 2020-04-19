34c2b2490d6d671be3aba4a11df9df2c939df66a
div-dm17 <1798dm@gmail.com>
2020-04-19 23:44:23 +0530

    hero-section add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
+// Loader
 let loader = document.querySelector("#loader");
 
 function loaderFn() {
     loader.style.display = "none";
 }
+
+// Nav Bar Active Tab Show
+let navBar = document.querySelector(".nav-links");
+let navItem = document.querySelectorAll(".nav-links > a")
+
+navItem.forEach(element => {
+    element.addEventListener('click', function(){
+        navBar.querySelector('.active').classList.remove('active');
+        element.classList.add('active');        
+    });
+});
